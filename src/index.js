801a831ba9bfa9e6cb5abf17d6f6a12ee422bee0
giancode1 <gieg5c@gmail.com>
2021-09-03 21:26:09 -0500

    Chat basico

--- src/index.js
+++ src/index.js
@@ -3,22 +3,28 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 
+const messages = [{
+    id: 1,
+    text: 'Bienvenido al chat',
+    author: 'Giancarlo',
+}]
+
 app.use(express.static(__dirname + '/public'))
 
 io.on('connection', (socket) => {
     console.log('a user connected:', socket.id)
 
-    socket.emit('messages', {
-        id: 1,
-        text: 'Hola soy un Mensaje',
-        author: 'Giancarlo',
-    })
+    socket.emit('server-messages', messages)
 
-    socket.on('client', data => console.log(data))
+    socket.on('client-message', (data) => {
+        console.log(data)
+        messages.push(data)
+        io.sockets.emit('server-messages',messages) //a todos los sockets les emite este evento
+    })  
 
 })
 
-server.listen(3000, ()=>{
+server.listen(3000, () => {
     console.log('Server on port', 3000)
 })
 
