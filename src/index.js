1d0ce1f632e9b6d29725e5b2db78e7005f734ed0
giancode1 <gieg5c@gmail.com>
2021-09-04 14:58:19 -0500

    deploy heroku

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 
+const PORT = process.env.PORT || 3000
 app.use(express.static(__dirname + '/public'))
 
 let socketsConected = new Set()
@@ -29,8 +30,8 @@ io.on('connection', (socket) => {
 
 })
 
-server.listen(3000, () => {
-    console.log('Server on port', 3000)
+server.listen(PORT, () => {
+    console.log(`Server on port ${PORT}`)
 })
 
 
